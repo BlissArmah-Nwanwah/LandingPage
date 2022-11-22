9daf85eb96e0775ba3653d51b82c17c1baa65263
BlissArmah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2022-11-21 20:52:36 -0800

    retouching styles on mobile responsiveness

--- src/Work/Page.js
+++ src/Work/Page.js
@@ -3,13 +3,13 @@ import "./page.css"
 import keyboard from './assets/desktop/image-keyboard.jpg'
 import key1 from './assets/desktop/image-phone-and-keyboard.jpg'
 import key2 from './assets/desktop/image-glass-and-keyboard.jpg'
+import battery from './assets/shared/icon-battery.svg'
 
 import { Button, Col, Container, Row } from 'react-bootstrap'
 const Page = () => {
     return (
-        <Container className='layout' >
-            <Row className="justify-content-space-between mt-3">
-                <Row xs={1} sm={2} className="center" >
+            <Row className="page-container">
+                <Row xs={1} sm={2} className="nav " >
 
                     <Col>
                         <>
@@ -18,12 +18,12 @@ const Page = () => {
                         </>
                     </Col>
                     <Col>
-                        <Button className='order-btn'>pre-order now</Button>
+                        <Button className='order-btn '>pre-order now</Button>
                     </Col>
                 </Row>
 
                 <Row xs={1} sm={2} className="mt-5 center">
-                    <Col className='hero-title'>
+                    <Col className='hero-title hero'>
                         <h1 className='title'>typemaster keyboard</h1>
                         <p className='title-text'>
                             Improve your productivity and gaming without breaking the bank. Upgrade to a high quality
@@ -37,7 +37,7 @@ const Page = () => {
                         </Row>
 
                     </Col>
-                    <Col >
+                    <Col className='hero2' >
                         <div className='img-container'>
                             <img src={keyboard} alt='keyboard' className='img-container' />
                         </div>
@@ -46,35 +46,64 @@ const Page = () => {
                 </Row>
                 <Row xs={1} md={2} className="mt-5" >
                     <Col>
-                    <div className='flex'>
+                        <div className='flex'>
 
-                    <div className='img-filter-container'>
+                            <div className='img-filter-container'>
 
-                        <img src={key1} className='img-filter' />
-                    </div>
-                    <div>
+                                <img src={key1} className='img-filter' />
+                            </div>
+                            <div className='pic'>
 
-                        <img src={key2} className='pic' />
-                    </div>
-                    </div>
+                                <img src={key2}  />
+                            </div>
+                        </div>
                     </Col>
 
                     <Col>
-                    <div className='key-container'>
+                        <div className='key-container'>
 
-                        <h1 className='key-title'>
-                            Mechanical wireless keyboard</h1>
-                        <p className='title-text'>
-                            The Typemaster keyboard boasts top-notch build and practical design. It offers a wide variety
-                            of switches and keycaps, along with reliable wireless connectivity.</p>
-                    </div>
+                            <h1 className='key-title'>
+                                Mechanical wireless keyboard</h1>
+                            <p className='title-info'>
+                                The Typemaster keyboard boasts top-notch build and practical design. It offers a wide variety
+                                of switches and keycaps, along with reliable wireless connectivity.</p>
+                        </div>
 
                     </Col>
 
                 </Row>
+                <Row md={4}>
+                    <Col className='property1'>
+                    <div className='svg-container'><img src={battery} /></div>
+                        <div><h1 className='poperty-title'>Highly compatible</h1></div>
+                        <div><p className='poperty-info'>Easy to use and works well with all major computer brands, gaming consoles and mobile devices. 
+  Plug & play, no installation or driver needed.</p></div>
+                    </Col>
+                    <Col className='property2'>
+                    <div className='svg-container'><img src={battery} /></div>
 
+                        <div><h1 className='poperty-title'>Highly compatible</h1></div>
+                        <div><p className='poperty-info'>Easy to use and works well with all major computer brands, gaming consoles and mobile devices. 
+  Plug & play, no installation or driver needed.</p></div>
+                    </Col>
+                    <Col className='property3'>
+                    <div className='svg-container'><img src={battery} /></div>
+
+                        <div><h1 className='poperty-title'>Highly compatible</h1></div>
+                        <div><p className='poperty-info'>Easy to use and works well with all major computer brands, gaming consoles and mobile devices. 
+  Plug & play, no installation or driver needed.</p></div>
+                    </Col>
+                    <Col className='property4'>
+                    <div className='svg-container'><img src={battery} /></div>
+                        <div><h1 className='poperty-title'>Highly compatible</h1></div>
+                        <div><p className='poperty-info'>Easy to use and works well with all major computer brands, gaming consoles and mobile devices. 
+  Plug & play, no installation or driver needed.</p></div>
+                    </Col>
+                </Row>
+            <div className='top-design'></div>
+            <div className='bottom-design'></div>
             </Row>
-        </Container>
+
 
     )
 }
