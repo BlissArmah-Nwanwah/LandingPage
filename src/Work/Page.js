5a0857544cead4b155fabacc1ce400203e5b66ae
BlissArmah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2022-11-23 18:54:54 -0800

    refactoring codes

--- src/Work/Page.js
+++ src/Work/Page.js
@@ -17,7 +17,7 @@ const Page = () => {
 
             <Row className='page'>
 
-                <Row  className="mt-5 flex-nav " >
+                <Row  className="mt-5 nav " >
 
                     <Col>
                        
@@ -27,67 +27,63 @@ const Page = () => {
                         </div>
                     </Col>
 
-                    <Col xs={6} md={3} lg={3} className='ci'>
+                    <Col xs={7} md={3} lg={3} className='nav-btn'>
                         <Button className='order-btn  '>Pre-order Now</Button>
                     </Col>
                 </Row>
 
-                <Row xs={1} sm={2} className="mt-5 center ">
+                <Row xs={1} sm={2} className="mt-5 hero-section ">
                     <Col xs={2}>
                         <div className=' hero' >
 
-                            <div className='hero-text'>
+                            <div>
 
-                                <h1 className='title'>typemaster keyboard</h1>
-                                <p className='title-text'>
+                                <h1 className='hero-title'>typemaster keyboard</h1>
+                                <p className='hero-text'>
                                     Improve your productivity and gaming without breaking the bank. Upgrade to a high quality
                                     mechanical typing experience.</p>
                             </div>
 
-                            <div className="title-footer">
+                            <div className="hero-footer">
 
                                 <Button className='hero-btn'>pre-order now</Button>
-                                <p className='text'>Release on 5/27</p>
+                                <p className='hero-footer-text'>Release on 5/27</p>
 
                             </div>
                         </div>
-
                     </Col>
-
                     <Col >
-                    <div className='img-container'>
-
+                    <div className='hero-img-container'>
                         <img src={keyboard} alt='keyboard'  />
                     </div>
                     </Col>
                 </Row>
-                <Row xs={2}  md={2} lg={3} className="mt-5 g-5  ff" >
+
+                <Row xs={2}  md={2} lg={3} className="about-section" >
                     <Col xs={3} sm={5} md={4} lg={3}>
-                        {/* <div className='flex'> */}
+                    
+                            <div className='about-first-image-container'>
 
-                            <div className='img-filter-container'>
-
-                                <img  src={key1} className='img-filter' />
+                                <img  src={key1} className='about-img' />
                             </div>
                             </Col>
 
                             <Col   lg={5}>
-                            <div className='picture-container'>
+                            <div className='about-picture-container'>
 
-                                <img src={key2} className='picture' />
+                                <img src={key2} className='about-picture' />
                             </div>
                             </Col>
-                        {/* </div> */}
                     
 
                     <Col sm={12} md={12}  >
                         <Row xs={1} sm={2} lg={1} className="p-5" >
                             <div  >
-                                <h1 className='key-title'>
+                                <h1 className='about-title'>
                                     Mechanical wireless keyboard</h1>
                             </div>
                             <div >
-                                <p className='title-info'>
+                                <p className='about-info'>
                                     The Typemaster keyboard boasts top-notch build and practical design. It offers a wide variety
                                     of switches and keycaps, along with reliable wireless connectivity.</p>
                             </div>
@@ -98,21 +94,21 @@ const Page = () => {
                     </Col>
 
                 </Row>
-                <Row xs={1} sm={2} md={2} className='mt-5 f' >
-                    <Col  className='property1'>
+                <Row xs={1} sm={2} md={2} className='mt-5 properties' >
+                    <Col  className='property'>
                         <div className='svg-container'><img src={tv} style={{ width: '35%', }} /></div>
                         <div><h1 className='poperty-title'>Highly compatible</h1></div>
                         <div><p className='poperty-info'>Easy to use and works well with all major computer brands, gaming consoles and mobile devices.
                             Plug & play, no installation or driver needed.</p></div>
                     </Col>
-                    <Col className='property2'>
+                    <Col className='property'>
                         <div className='svg-container'><img src={bluetooth} style={{ width: '25%' }} /></div>
 
                         <div><h1 className='poperty-title'>Wireless with bluetooth</h1></div>
                         <div><p className='poperty-info'>Powerful 2.4G RF technology allows you to connect the cordless keyboard from up to 30ft away.
                             Simply plug the unifying receiver into your computer.</p></div>
                     </Col>
-                    <Col className='property3'>
+                    <Col className='property'>
                         <div className='svg-container'><img src={battery} style={{ width: '35%', }} /></div>
 
                         <div><h1 className='poperty-title'> High capacity battery</h1></div>
@@ -120,7 +116,7 @@ const Page = () => {
                             Enjoy 40 hours of usage time between charges.
                         </p></div>
                     </Col>
-                    <Col className='property4'>
+                    <Col className='property'>
                         <div className='svg-container'><img src={light} style={{ width: '40%', }} /></div>
                         <div><h1 className='poperty-title'>RGB backlit modes</h1></div>
                         <div><p className='poperty-info'>Choose from 4 backlight brightness levels and adjustable breathing speed. Each key glows intensely in the dark and helps
@@ -130,8 +126,8 @@ const Page = () => {
                 </Row>
                 
                 
-                <footer className='foot'>
-                <p className='footer'><span className='footer-span'>Typemaster 2021 | </span> All Rights Reserved</p>
+                <footer >
+                <p ><span className='footer-span'>Typemaster 2021 | </span> All Rights Reserved</p>
                 </footer>
                 
 
